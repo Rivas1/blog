7914dfb75db6a17fc9b6f179c88289788a32f784
Rivas1 <rivas1@cpp.edu>
2018-12-17 13:44:08 -0800

    CRU ready to go
    Added Create, Read, Update functionality from CRUD

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
 	validates :title, presence: true, #all articles mustd have a title that is at least five characters long. 
-					  length: { minimum: 6 }
+					  length: { minimum: 5 }
 end
